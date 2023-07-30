312fdbb4b9c5f90b97b3eb49f1e8b490d827704e
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2023-07-29 20:56:43 -0600

    varaibles de entorno, intelligense de typescript y axios instance

--- src/models/busquedas.ts
+++ src/models/busquedas.ts
@@ -1,13 +1,27 @@
 import axios from "axios";
+import "dotenv/config";
 
 export class Busquedas {
   private historial: string[] = [];
 
   constructor() {}
 
+  get paramsMapbox() {
+    return {
+      language: "es",
+      access_token: process.env.MAPBOX,
+      limit: 5,
+    };
+  }
+
   async ciudad(lugar: string) {
     try {
-      const res = await axios.get("");
+      const instance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+        params: this.paramsMapbox,
+      });
+      const res = await instance.get("");
+      console.log(res.data);
       return [];
     } catch (error) {
       return [];
